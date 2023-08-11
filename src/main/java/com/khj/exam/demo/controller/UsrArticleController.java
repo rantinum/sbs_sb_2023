645cb48ff949732b7b90de80cf12396aa3307716
kimhyeji <gpwl5589@gmail.com>
2023-08-11 09:30:14 +0900

    getArticle, delteArticle 서비스 메서드로 게시물 삭제기능 구현

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -11,9 +11,12 @@ import com.khj.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
+	// 인스턴스 변수 시작
 	private int articleLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 	
+	// 생성자
 	public UsrArticleController() {
 		articleLastId = 0;
 		articles = new ArrayList<>();
@@ -21,6 +24,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 	
+	// 서비스 메서드 시작
 	private void makeTestData() {
 		for ( int i = 1; i <= 10; i++ ) {
 			String title = "제목" + i;
@@ -40,7 +44,26 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	private Article getArticle(int id) {
+		for ( Article article : articles ) {
+			if ( article.getId() == id ) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+		
+	}
+	// 서비스 메서드 끝
 
+	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -53,4 +76,20 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시물이 삭제되었습니다.";
+	}
+	// 액션 메서드 끝
+	
 }
