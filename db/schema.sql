5161065056921b6af9b163cca640a8e55f8b99d8
kimhyeji <gpwl5589@gmail.com>
2023-08-11 11:43:07 +0900

    db/schema, data sql 추가

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+# DB 생성
+DROP DATABASE IF EXISTS sb_proj_2023_t;
+CREATE DATABASE sb_proj_2023_t;
+USE sb_proj_2023_t;
+
+# 게시물 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+SELECT * FROM article;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 1',
+`body` = '내용 1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 2',
+`body` = '내용 2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 3',
+`body` = '내용 3';
--- src/main/java/com/khj/exam/demo/service/ArticleService.java
+++ src/main/java/com/khj/exam/demo/service/ArticleService.java
@@ -2,7 +2,6 @@ package com.khj.exam.demo.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.khj.exam.demo.repository.ArticleRepository;
