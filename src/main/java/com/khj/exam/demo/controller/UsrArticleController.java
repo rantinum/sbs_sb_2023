4630b6b1c1a741fb954c216d337195c5800f179d
kimhyeji <gpwl5589@gmail.com>
2023-08-11 09:07:08 +0900

    writeArticle 메서드를 통해 중복 로직 제거

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -23,20 +23,14 @@ public class UsrArticleController {
 	
 	private void makeTestData() {
 		for ( int i = 1; i <= 10; i++ ) {
-			int id = articleLastId + 1;
 			String title = "제목" + i;
 			String body = "내용" + i;
 			
-			Article article = new Article(id, title, body);
-			
-			articles.add(article);
-			articleLastId = id;
+			writeArticle(title, body);
 		}
 	}
-
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	
+	public Article writeArticle(String title, String body) {
 		int id = articleLastId + 1;
 		
 		Article article = new Article(id, title, body);
@@ -44,6 +38,13 @@ public class UsrArticleController {
 		articles.add(article);
 		articleLastId = id;
 		
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
 		return article;
 	}
 	
