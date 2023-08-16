213d2d6990559d7944d07d935d411d519e8e94e6
rantinum <syjcode5724@gmail.com>
2023-08-16 10:48:00 +0900

    GIT, usr/article/getArticle에 ResultData 적용

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.khj.exam.demo.service.ArticleService;
+import com.khj.exam.demo.util.Ut;
 import com.khj.exam.demo.vo.Article;
+import com.khj.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -27,20 +29,22 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
-		return articleService.getArticles();
+	public ResultData getArticles() {
+		List<Article> articles = articleService.getArticles();
+		
+		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
 	}
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.",id));
 		}
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물 입니다.", id), article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
