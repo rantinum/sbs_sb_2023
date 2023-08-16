58e452592bbeb52b7a28bc69aa977e2d6ba32428
rantinum <syjcode5724@gmail.com>
2023-08-16 13:45:23 +0900

    GIT, 게시물 삭제 시 권한체크

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -23,31 +23,31 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
 		boolean isLogined = false;
-		
+
 		int loginedMemberId = 0;
-		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
 			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
-		
+
 		if (isLogined == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
-		
+
 		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 작성해주세요.");
 		}
 		if (Ut.empty(body)) {
 			return ResultData.from("F-2", "body(을)를 작성해주세요.");
 		}
-		
+
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
-		
+
 		int id = writeArticleRd.getData1();
-		
+
 		Article article = articleService.getArticle(id);
-		
+
 		return ResultData.newData(writeArticleRd, article);
 	}
 
@@ -55,7 +55,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
-		
+
 		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
 	}
 
@@ -65,7 +65,7 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.",id));
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
 		return ResultData.from("S-1", Ut.f("%d번 게시물 입니다.", id), article);
@@ -73,8 +73,25 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData<Integer> doDelete(int id) {
+	public ResultData<Integer> doDelete(HttpSession httpSession, int id) {
+		boolean isLogined = false;
+
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+		
 		Article article = articleService.getArticle(id);
+		
+		if ( article.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
 
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
